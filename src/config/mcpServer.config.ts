bf40f8f827498e960b554a6d4957e3e9f138596f
Prince Raj <dev.prince636@gmail.com>
2026-06-13 23:21:02 +0530

    Update mcpServer.config.ts

--- src/config/mcpServer.config.ts
+++ src/config/mcpServer.config.ts
@@ -1,8 +1,10 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
 const server = new McpServer({
   name: "pr-context-engine",
   version: "1.0.0",
   description:
     "PR analysis engine with RAG capabilities using GitHub & Pinecone",
 });
+
 export default server;
